fce16ac15597c287c3d6831178d9761c3ed44fa4
Bruno Pivoto <brunorangel@gec.inatel.br>
2023-05-26 11:26:58 -0300

    initial commit

---
+++ shop/lib/models/order_list.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:shop/models/cart.dart';
+import 'package:shop/models/cart_item.dart';
+
+import '../utils/constants.dart';
+import 'order.dart';
+
+class OrderList with ChangeNotifier {
+  // ignore: prefer_final_fields
+  List<Order> _items = [];
+
+  List<Order> get items {
+    return [..._items];
+  }
+
+  int get itemsCount {
+    return items.length;
+  }
+
+  Future<void> loadOrders() async {
+    _items.clear();
+    final response =
+        await http.get(Uri.parse('${Constants.ORDERS_BASE_URL}.json'));
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((orderId, orderData) {
+      _items.add(Order(
+        id: orderId,
+        date: DateTime.parse(orderData['date']),
+        total: orderData['total'],
+        products: (orderData['products'] as List<dynamic>).map((item) {
+          return CartItem(
+            id: item['id'],
+            productId: item['productId'],
+            name: item['name'],
+            qnt: item['qnt'],
+            price: item['price'],
+          );
+        }).toList(),
+      ));
+    });
+    notifyListeners();
+  }
+
+  Future<void> addOrder(Cart cart) async {
+    final date = DateTime.now();
+    final response = await http.post(
+      Uri.parse('${Constants.ORDERS_BASE_URL}.json'),
+      body: jsonEncode(
+        {
+          'total': cart.totalAmount,
+          'date': date.toIso8601String(),
+          'products': cart.items.values
+              .map(
+                (cartItem) => {
+                  'id': cartItem.id,
+                  'productId': cartItem.productId,
+                  'name': cartItem.name,
+                  'qnt': cartItem.qnt + 1,
+                  'price': cartItem.price,
+                },
+              )
+              .toList(),
+        },
+      ),
+    );
+
+    final id = jsonDecode(response.body)['name'];
+    _items.insert(
+      0,
+      Order(
+        id: id,
+        total: cart.totalAmount,
+        products: cart.items.values.toList(),
+        date: date,
+      ),
+    );
+    notifyListeners();
+  }
+}
