bd18232de5a98f0982f11be9be6a2e041371459f
Bruno Pivoto <brunorangel@gec.inatel.br>
2023-05-30 13:09:18 -0300

    logout fix

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -78,7 +78,9 @@ class Auth with ChangeNotifier {
     _userId = null;
     _expiryDate = null;
     _clearLogoutTimer();
-    notifyListeners();
+    Store.remove('userData').then((_) {
+      notifyListeners();
+    });
   }
 
   void _clearLogoutTimer() {
